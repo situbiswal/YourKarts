161934c2f9c98b4f68afaf021c5ec620a7cffdfc
situ biswal <situbiswal123@gmail.com>
2021-12-14 22:18:51 +0530

    second commit

--- Filpkart/settings.py
+++ Filpkart/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
+DEBUG =False
 
 ALLOWED_HOSTS = ["yourcarts.herokuapp.com","localhost"]
-
+CSRF_TRUSTED_ORIGINS = ['yourcarts.herokuapp.com']
 
 # Application definition
 
